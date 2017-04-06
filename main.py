5203676017f07ed264d88a361eb72d75d14ba826
YuSangHuck <g01099387636@gmail.com>
2017-04-06 16:39:56 +0900

    real main.py

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ out_dataset_dir = Dataset.out_dataset_dir # r'D:\Github\dataset\img\mydataset\ou
 out_model_dir = Train.out_model_dir
 
 if __name__ == '__main__':
-    Dataset.Dataset_create(my_dataset_dir,out_dataset_dir,32,32,framework=3)
+    Dataset.Dataset_create(in_dataset_dir = my_dataset_dir,
+                           out_dataset_dir = out_dataset_dir,
+                           row = 32,
+                           col = 32,
+                           framework=3)
     print(Dataset.Dataset_result(out_dataset_dir))
-    Train.Train_create(dataset_dir=out_dataset_dir, framework=3, out_model_dir=out_model_dir, max_epochs=20, mb_size=300, network_name='conv')
+    Train.Train_create(dataset_dir = out_dataset_dir,
+                       framework = 3, 
+                       out_model_dir = out_model_dir, 
+                       max_epochs = 3, 
+                       mb_size = 300, 
+                       network_name = 'conv')
     print(Train.Train_result(out_model_dir))
